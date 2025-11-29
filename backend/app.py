c4b177a339379e9240934bea42295612c3a48f02
Prerna Sherki <prernasherki2310@gmail.com>
2025-11-30 00:08:42 +0530

    changes in app

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,10 @@
-from flask import Flask,request,jsonify,send_from_directory
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import similarity
-# from auth import auth
 
-
-
-app = Flask(__name__, static_folder='build', static_url_path='/')
+app = Flask(__name__)
 CORS(app)
 
-# app.register_blueprint(auth)
-@app.route('/')
-def serve_react():
-    return send_from_directory(app.static_folder, 'index.html')
 @app.post("/upload")
 def upload():
     files = request.files.getlist("files")
@@ -22,6 +15,3 @@ if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
-from flask_cors import CORS
-CORS(app)
